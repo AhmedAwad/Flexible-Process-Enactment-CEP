8575276b8033839ed4720830d4d3f3c5a5d07cf7
Ahmed Awad <ahmad.gaafar@gmail.com>
2023-03-13 13:27:31 +0200

    completing the generation template for one-level loop and making an example process model rules

---
+++ src/main/java/ee/ut/dsg/process/encatment/cep/Runner.java
@@ -0,0 +1,23 @@
+package ee.ut.dsg.process.encatment.cep;
+
+import java.text.Format;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.charset.Charset;
+import java.util.Date;
+
+import com.espertech.esper.common.client.EPCompiled;
+import com.espertech.esper.common.client.configuration.Configuration;
+import com.espertech.esper.compiler.client.CompilerArguments;
+import com.espertech.esper.compiler.client.EPCompileException;
+import com.espertech.esper.compiler.client.EPCompiler;
+import com.espertech.esper.compiler.client.EPCompilerProvider;
+import com.espertech.esper.runtime.client.*;
+
+
+public class Runner {
+}
